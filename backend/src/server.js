352813ddef8c908a284fbb01d4abb6f5775402e3
Drorka~ <drorpa4@gmail.com>
2023-08-16 12:34:28 +0300

    update start server func

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,21 +7,19 @@ const { mongoConnect } = require("./db");
 
 const http = require("http");
 const server = http.createServer(app);
-const socketIOSetup = require("./sockets/index");
+const socketIOSetup = require("./sockets/socket");
 
 app.use(express.json());
-app.use(
-	cors({
-		origin: [
-			"http://localhost:8080",
-			"http://localhost:3000",
-			"http://localhost:5173",
-			"https://code-web-app.onrender.com/",
-		],
-	})
-);
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+} else {
+	app.use(
+		cors({
+			origin: ["http://localhost:5173"],
+		})
+	);
+}
 
 app.use("/api", codeBlocksRoutes);
 
@@ -29,12 +27,15 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
 });
 
-mongoConnect(() => {
+const startServer = async () => {
+	await mongoConnect();
 	const PORT = process.env.PORT || 3030;
 	server.listen(PORT, () => {
 		console.log(`Server is running on port ${PORT}`);
 	});
 	socketIOSetup(server);
-});
+};
+
+startServer();
 
 module.exports = { app };
