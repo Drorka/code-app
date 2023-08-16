176f0567248af69a56042dfc44134305a8f515f6
Drorka~ <drorpa4@gmail.com>
2023-08-16 12:35:33 +0300

    update vite configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	define: {
+		"import.meta.env.VITE_BASE_API_DEV": JSON.stringify(
+			"//localhost:3030/api/"
+		),
+		"import.meta.env.VITE_BASE_API_PROD": JSON.stringify(
+			"https://code-app-dszr.onrender.com/api/"
+		),
+		"import.meta.env.VITE_BASE_URL_DEV": JSON.stringify("//localhost:3030/"),
+		"import.meta.env.VITE_BASE_URL_PROD": JSON.stringify(
+			"https://code-app-dszr.onrender.com/"
+		),
+	},
+});
+
+// ********************************
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
